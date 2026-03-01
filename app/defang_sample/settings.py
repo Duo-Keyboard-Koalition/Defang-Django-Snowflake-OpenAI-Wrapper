79ee0da6d411416e89874fcc600759e265d0e246
Darcy Liu <darcy.ldx@gmail.com>
2026-03-01 02:57:44 -0500

    chore: remove nationstates_app entirely (#4)
    Co-authored-by: NomeClaw <awesomefalcon1@gmail.com>

--- app/defang_sample/settings.py
+++ app/defang_sample/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'example_app',
-    'nationstates_app',
     'chat_app',
     'cortex_app',
     'frontend_app',
